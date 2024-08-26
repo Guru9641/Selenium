8ddb43dc8a085cea8f9e313facdbc6dc7dab2f4d
Guru9641 <kumaraguru418@gmail.com>
2024-08-26 16:12:55 +0530

    Second day class code2

---
+++ Array.java
@@ -0,0 +1,26 @@
+package week2.day2;
+
+public class Array {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] age = {20, 25, 35, 55, 66, 74};
+		
+		int length = age.length;
+		System.out.println("Total number of length:"+length);
+		System.out.println(age [3]);
+		System.out.println(age [length-1]);
+		
+		
+				
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
