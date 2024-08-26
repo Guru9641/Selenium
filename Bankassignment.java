347422c47225feb60a839055d13108b90709d5bb
Guru9641 <kumaraguru418@gmail.com>
2024-08-26 16:12:12 +0530

    Second day class code

---
+++ Bankassignment.java
@@ -0,0 +1,25 @@
+package week2.day2;
+
+public class Bankassignment {
+	
+	
+	String employeeName= "Guru";
+	String comapnyname= "ZR";
+	int empID= 9641;
+	int salary=70000;
+	float productionrate= 80f;
+	
+	public static void main(String[] args) {
+		
+		Bankassignment b1= new Bankassignment();
+		System.out.println(b1.employeeName);
+		System.out.println(b1.comapnyname);
+		System.out.println(b1.empID);
+		System.out.println(b1.salary);
+		System.out.println(b1.productionrate);
+		
+		
+
+	}
+
+}
